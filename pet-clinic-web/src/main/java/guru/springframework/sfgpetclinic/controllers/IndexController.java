7ae5afaff6d75799ef0d5c25b5ac045e5c11b561
Jayamaruthyraman Jaganathan <jayjag04@gmail.com>
2020-08-18 19:42:45 -0400

    Fixed all broken links.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -2,11 +2,17 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     @GetMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String findOwners() {
+        return "notimplemented";
     }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @GetMapping({"/vets", "/vets/index","/vets.html", "/vets/index.html"})
 
     public String listVets(Model model) {
 
